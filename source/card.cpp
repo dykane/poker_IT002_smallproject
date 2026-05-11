b7ce0ab8ec0043cf4f2c48b43c576a70d1ba65d2
Hoàng Duy khánh <252594383+dykane@users.noreply.github.com>
2026-05-11 10:47:45 +0700

    Update card.cpp
    Added toString method to convert Card to string representation.

--- source/card.cpp
+++ source/card.cpp
@@ -1,6 +1,11 @@
 #include "poker.h"
 
-Card::Card(int r, char s) : rank(r), suit(s) {}
-bool Card::operator<(const Card& other) const {
-    return rank < other.rank;
+string Card::toString() const {
+    string r = "";
+    if (rank <= 10) r = to_string(rank);
+    else if (rank == 11) r = "J";
+    else if (rank == 12) r = "Q";
+    else if (rank == 13) r = "K";
+    else if (rank == 14) r = "A";
+    return r + suit;
 }
